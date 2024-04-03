7948cdbc439e43554e21920f5920ade41bef8743
philipszdavido <kurtwanger40@gmail.com>
2024-04-03 12:41:28 +0100

    Initial Commit

---
+++ networt/BankInfo.swift
@@ -0,0 +1,26 @@
+//
+//  BankInfo.swift
+//  networt
+//
+//  Created by Chidume Nnamdi on 4/7/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class BankInfo {
+    var amount: Int;
+    var bankName: String;
+    var currency: String;
+    var number: Int;
+    
+    init(amount: Int, bankName: String, currency: String, number: Int) {
+        self.amount = amount
+        self.bankName = bankName
+        self.currency = currency
+        self.number = number
+    }
+    
+}
+
---
+++ networt/SettingsView.swift
@@ -0,0 +1,55 @@
+//
+//  SettingsView.swift
+//  networt
+//
+//  Created by Chidume Nnamdi on 4/7/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    @ObservedObject var settings: GlobalSettings;
+    
+    var body: some View {
+        
+        NavigationStack {
+            List {
+                                
+                Section("Currency") {
+                    HStack {
+                        Text("Global Currency")
+                        Picker(selection: $settings.currency, label: Text("")) {
+                            ForEach(currenciesWithFlags, id: \.0) { name, flag in
+                                            Text("\(name) \(flag)")
+                                        }
+                                    }
+                                    .pickerStyle(DefaultPickerStyle())
+                                    
+                    }
+                }
+                
+                Toggle(isOn: $settings.showMyBanks) {
+                    Text("Show My Banks")
+                }
+                
+                Toggle(isOn: $settings.hideNetworth) {
+                    Text("Hide Networth")
+                }
+                
+                Toggle(isOn: $settings.showUpdates) {
+                    Text("Show Updates")
+                }
+                
+            }
+            .navigationTitle("Settings")
+        }
+        
+    }
+}
+
+#Preview {
+    
+    SettingsView(settings: GlobalSettings())
+    
+}
